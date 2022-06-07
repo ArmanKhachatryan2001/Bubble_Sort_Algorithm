b4ef93604111ad65a5c7f413e3d69a54f6bb1a7c
ArmanKhachatryan2001 <104670251+ArmanKhachatryan2001@users.noreply.github.com>
2022-06-07 16:31:44 +0400

    Update bst_sort_algorithm.cpp

--- bst_sort_algorithm.cpp
+++ bst_sort_algorithm.cpp
@@ -1,35 +1,36 @@
 #include <iostream>
 
 template <typename iterator>
-void bubbl_sort(iterator begin, iterator end)
+void bubble_sort(iterator begin, iterator end)
 {
     bool true_ = true;
-    for(iterator i = begin; i != end; ++i) {
-    for(iterator j = begin; j < i; ++j) {
-    if (*i < *j) {
-    true_ = false;
-    std::swap(*i, *j);
-    }
-    }
-    if(true_) {return;}
+    for(iterator i = begin ; i != end; ++i) {
+        for(iterator j = begin; j < end; ++j) {
+            if (*i < *j) {
+                true_ = false;
+                std::swap(*i, *j);
+            }
+        }
+        if(true_) {return;}
     }
 }
 template <typename T>
 void sort(T* arr,int size)
 {
+    bool _true = true;
     for(int i = 0; i < size; ++i) {
-    int j = i;
-    while(j < size){
-    if(arr[j] > arr[j+1]) {
-    T my_swap = arr[j];
-    arr[j] = arr[j+1];
-    arr[j+1] = my_swap;
-    }
-    ++j;
-    }
+        for(int j = 0;j < size;++j) {
+            if(arr[j] > arr[j+1]) {
+                std::swap(arr[j],arr[j+1]);
+                _true = false;
+            }
+        }
+        if(_true) {
+            break;
+        }
     }
 }
 int main()
 {
-    return 0;
+
 }
